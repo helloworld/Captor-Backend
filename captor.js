66fe933b3d7f0ad4be7347587a09576851b7982b
Sashank Thupukari <sthupukari@gmail.com>
2014-11-06 17:47:15 -0500

    New Date + Images Route

--- captor.js
+++ captor.js
@@ -3,7 +3,6 @@ Messages = new Meteor.Collection('messages');
 Router.route('/', function() {
     this.render('Home');
 });
-
 if (Meteor.isServer) {
     Router.route('/api/data', {
         where: 'server'
@@ -11,7 +10,6 @@ if (Meteor.isServer) {
         var req = this.request;
         var res = this.response;
         var query = req.query;
-
         if (query.username) {
             if (Clients.findOne({
                     username: query.username
@@ -42,13 +40,18 @@ if (Meteor.isServer) {
                 }) == undefined) {
                 res.end("User does not exist");
             } else {
+                date = new Date;
                 Clients.update({
                     username: query.username
                 }, {
                     $push: {
                         history: {
                             text: query.string,
-                            date: new Date()
+                            date: {
+                                day: date.getDate() + "",
+                                month:date.getMonth() + "",
+                                year: date.getFullYear() + "",
+                            }
                         }
                     }
                 });
@@ -59,29 +62,84 @@ if (Meteor.isServer) {
             res.end("Error: Invalid arguments for new user");
         }
     });
-
-    Router.route('/api/createUser
-', {
+    Router.route('/api/img', {
         where: 'server'
-    }).post(function() {
+    }).get(function() {
         var req = this.request;
         var res = this.response;
         var query = req.query;
-
         if (query.username) {
             if (Clients.findOne({
                     username: query.username
                 }) == undefined) {
-                Clients.insert({
-                    username: query.username,
-                    history: []
-                });
-                res.end("User " + query.username + " created");
+                res.end("User does not exist");
             } else {
-                res.end("User already exists.")
+                var requestedUser = Clients.findOne({
+                    username: query.username
+                })
+                console.log(requestedUser);
+                requestedImages = requestedUser.history;
+                images = {
+                    images: requestedImages
+                }
+                requestedImages = JSON.stringify(requestedImages);
+                res.end(requestedImages)
             }
         } else {
             res.end("Error: Invalid arguments for new user");
         }
-    })
+    }).post(function() {
+        var req = this.request;
+        var res = this.response;
+        var query = req.query;
+        console.log(req)
+        if (query.username && query.string) {
+            if (Clients.findOne({
+                    username: query.username
+                }) == undefined) {
+                res.end("User does not exist");
+            } else {
+                Clients.update({
+                    username: query.username
+                }, {
+                    $push: {
+                        images: {
+                            text: query.string,
+                            date: {
+                                day: date.getDate() + "",
+                                month:date.getMonth() + "",
+                                year: date.getFullYear() + "",
+                            }
+                        }
+                    }
+                });
+                res.end(query.string + " added");
+            }
+        } else {
+            console.log(query)
+            res.end("fail")
+        }
+    });
+    Router.route('/api/createUser', {
+        where: 'server'
+    }).post(function() {
+    var req = this.request;
+    var res = this.response;
+    var query = req.query;
+    if (query.username) {
+        if (Clients.findOne({
+                username: query.username
+            }) == undefined) {
+            Clients.insert({
+                username: query.username,
+                history: []
+            });
+            res.end("User " + query.username + " created");
+        } else {
+            res.end("User already exists.")
+        }
+    } else {
+        res.end("Error: Invalid arguments for new user");
+    }
+})
 }
